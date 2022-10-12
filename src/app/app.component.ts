aa76784e33a20bc18792546fd603d93a5aa9b681
unknown <jouinikk1@gmail.com>
2022-10-12 18:50:13 +0100

    css changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-
+import { Car } from './car';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'ex2';
-}
+selector: 'app-root',
+templateUrl: './app.component.html',
+styles: []
+ })
+ export class AppComponent {
+  title="ex2";
+ _cars = [
+ new Car('car1', 2002, 'bmw', 'm3','https://en.wikipedia.org/wiki/BMW_M3'),
+ new Car('car2', 2017, 'acura', 'nsx','https://en.wikipedia.org/wiki/Honda_NSX'),
+ new Car('car3', 2016, 'chevy', 'camaro','https://en.wikipedia.org/wiki/Chevrolet_Camaro')
+];
+ showCar(event:any){
+ const desc = event.target.parentElement.dataset.desc;
+ if (window.confirm('If you click "ok" you would be redirected to an article about the ' +
+desc + '. Cancel will load this website ')){
+ window.location.href=event.target.parentElement.
+ dataset.article;
+ }
+ }
+} 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Car } from './car';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
